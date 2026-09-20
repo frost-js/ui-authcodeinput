583467dfc7f042c63c0bf1de150b1f8f6f41954b
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2026-09-20 18:17:38 +1000

    Remove redundant fQuery global assignment from test fixture

--- test/support/test.js
+++ test/support/test.js
@@ -23,7 +23,6 @@ const test = base.extend({
                     throw new Error('Failed to initialize AuthCodeInput on the test page.');
                 }
 
-                window.$ = window.fQuery;
                 document.body.replaceChildren();
             });
 
